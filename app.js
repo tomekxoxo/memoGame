417d2efe6f2df93a4012e7ea6aed60a55b71378f
tomasz kasprowicz <tomaszkasprowicz24@gmail.com>
2020-07-29 22:35:25 +0200

    finished game

--- app.js
+++ app.js
@@ -1,106 +1,115 @@
 let clickCount = 0;
+let moveCount = 0;
 let table = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 let compareImg = [];
 let compareId = [];
 let correctImages = [];
 const memos = {
-  1:'bird_green',
-  2:'bird_yellow',
-  3:'cat',
-  4:'cow',
-  5:'dog',
-  6:'fish_purple',
-  7:'fish_yellow',
-  8:'owl',
-}
-window.addEventListener('load', populateBoard);
+  1: "bird_green",
+  2: "bird_yellow",
+  3: "cat",
+  4: "cow",
+  5: "dog",
+  6: "fish_purple",
+  7: "fish_yellow",
+  8: "owl",
+};
+window.addEventListener("load", populateBoard);
 
 const board = document.querySelector(".game__board");
-board.addEventListener("click",rotate);
+board.addEventListener("click", rotate);
 
 function rotate(e) {
-    if (clickCount < 2) {
-      if (e.target.classList.contains("tile__front")) {
-        const img = e.target.nextElementSibling.firstElementChild.src;
-        const id = e.target.parentElement.parentElement.id;
-        const tile = document.getElementById(id);
-        tile.firstElementChild.classList.add("rotate");
-        clickCount++;
-        compareImg.push(img)
-        compareId.push(id)
-        if (clickCount == 2) {
-          checkCount(compareId,compareImg)    
-        }
+  if (clickCount < 2) {
+    if (e.target.classList.contains("tile__front")) {
+      const img = e.target.nextElementSibling.firstElementChild.src;
+      const id = e.target.parentElement.parentElement.id;
+      const tile = document.getElementById(id);
+      tile.firstElementChild.classList.add("rotate");
+      clickCount++;
+      compareImg.push(img);
+      compareId.push(id);
+      if (clickCount == 2) {
+        checkCount(compareId, compareImg);
       }
-      if (e.target.classList.contains("tile__inner")) {
-        const img = e.target.firstElementChild.nextElementSibling.firstElementChild.src;
-        const id = e.target.parentElement.id;
-        const tile = document.getElementById(id);
-        tile.firstElementChild.classList.add("rotate");
-        clickCount++;
-        compareImg.push(img)
-        compareId.push(id)
-        if (clickCount == 2) {
-          checkCount(compareId,compareImg)
-        }
-      }
-      
     }
+    if (e.target.classList.contains("tile__inner")) {
+      const img =
+        e.target.firstElementChild.nextElementSibling.firstElementChild.src;
+      const id = e.target.parentElement.id;
+      const tile = document.getElementById(id);
+      tile.firstElementChild.classList.add("rotate");
+      clickCount++;
+      compareImg.push(img);
+      compareId.push(id);
+      if (clickCount == 2) {
+        checkCount(compareId, compareImg);
+      }
+    }
+  }
 }
 
 function shuffle(a) {
   for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
   }
   return a;
 }
 
 function checkCount(ids, imgs) {
+  moveCount++;
   setTimeout(() => {
     if (imgs[0] == imgs[1]) {
-      correctImages.push(parseInt(ids[0]), parseInt(ids[1]))
+      correctImages.push(parseInt(ids[0]), parseInt(ids[1]));
       document.querySelectorAll(".tile").forEach((tile) => {
-        correctImages.forEach(element => {
+        correctImages.forEach((element) => {
           if (element == parseInt(tile.id)) {
-            return
+            return;
           }
-        })    
+        });
       });
-    }
-    else {
+    } else {
       if (correctImages.length <= 0) {
-        document.querySelectorAll('.tile').forEach(tile => {
-            tile.firstElementChild.className = "tile__inner"; 
-        })
-      }
-      else {
-        document.querySelectorAll('.tile').forEach(tile => {
-          for (let key in correctImages) {
-            if (correctImages[key] != parseInt(tile.id)) {
-              document.getElementById(tile.id).firstElementChild.className = 'tile__inner'
-            }
-            else {
-              continue
-            }
+        document.querySelectorAll(".tile").forEach((tile) => {
+          tile.firstElementChild.className = "tile__inner";
+        });
+      } else {
+        document.querySelectorAll(".tile").forEach((tile) => {
+          if (correctImages.find((e) => e == tile.id)) {
+            document.getElementById(tile.id).firstElementChild.className =
+              "tile__inner rotate";
+          } else {
+            document.getElementById(tile.id).firstElementChild.className =
+              "tile__inner";
           }
-        })
-      }    
+        });
+      }
     }
     clickCount = 0;
     compareImg = [];
     compareId = [];
+    if (correctImages.length == 16) {
+      showModal()
+    }
   }, 1300);
 }
 
 function populateBoard() {
   shuffle(table);
-  document.querySelectorAll('.tile__back').forEach((tile, index) => {
-    const img = document.createElement('img');
+  document.querySelectorAll(".tile__back").forEach((tile, index) => {
+    const img = document.createElement("img");
     const link = memos[table[index]];
-    img.setAttribute('src', `/memos/${link}.png`);
-    img.setAttribute('alt', `image`);
+    img.setAttribute("src", `/memos/${link}.png`);
+    img.setAttribute("alt", `image`);
     tile.appendChild(img);
-  })
+  });
 }
 
+function showModal() {
+  document.querySelector('.move-count').innerText = `moves: ${moveCount}`;
+  document.querySelector('.modal-container').classList.add('show');
+  document.querySelector('.replay').addEventListener('click', () => {
+    location.reload()
+  })
+}
